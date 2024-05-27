f4426326810c35b7c80f2a2b555da48c4ed16e73
Asus <Asus@DESKTOP-NQR04QO.bbrouter>
2024-05-27 17:37:07 +0530

    First change

---
+++ SampleProject/src/main/java/com/DSA/pro/Node.java
@@ -0,0 +1,34 @@
+package com.DSA.pro;
+
+
+
+public class Node {
+	int data;
+	Node next;
+	
+	
+	public static void main(String[] args)
+	{
+		Node head,middle,last;
+		head=new Node();
+		middle=new Node();
+		last=new Node();
+		
+		head.data=10;
+		middle.data=20;
+		last.data=30;
+		
+		head.next=middle;
+		middle.next=last;
+		last.next=null;
+		
+		Node temp=head;
+		while(temp !=null)
+		{
+			System.out.println(temp);
+			temp=temp.next;
+		}
+		
+	}
+
+}
